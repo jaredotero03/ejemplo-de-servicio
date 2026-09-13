1e7641a26f2819a0058562185c2e0540c34d040b
Johan Jared Otero Guzmán <jaredotero03@gmail.com>
2026-09-13 15:50:07 -0600

    presentación update v1.0

--- servicio.php
+++ servicio.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Hola";
+echo "Ávila Peralta Diego Rafael\nCarvente Miguel Isaí\nContretas Gama Tadeo Habith\nGonzález Rivera Axel\nGuzmán Sánchez Luis Enrique\nOrtega Maldonado Rodrigo\nOtero Guzmán Johan Jared\nPérez Arellano Ariana\nRamírez López Marilú\nTrejo Garrido Ángel Gallagert";
