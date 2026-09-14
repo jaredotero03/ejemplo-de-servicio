efe9317b29cf20f9d208a57285f59037bf976c23
Johan Jared Otero Guzmán <jaredotero03@gmail.com>
2026-09-13 23:46:14 -0600

    correción_nombres_update v1.0

--- servicio.php
+++ servicio.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Ávila Peralta Diego Rafael\nCarvente Miguel Isaí\nContretas Gama Tadeo Habith\nGonzález Rivera Axel\nGuzmán Sánchez Luis Enrique\nOrtega Maldonado Rodrigo\nOtero Guzmán Johan Jared\nPérez Arellano Ariana\nRamírez López Marilú\nTrejo Garrido Ángel Gallagert";
+echo "Ávila Peralta Diego Rafael\nCarvente Miguel Isaí\nContreras Gama Tadeo Habith\nGonzález Rivera Axel\nGuzmán Sánchez Luis Enrique\nOrtega Maldonado Rodrigo\nOtero Guzmán Johan Jared\nPérez Arellano Ariana\nRamírez López Marilú\nTabárez López Héctor Jesús\nTrejo Garrido Ángel Gallagert";
