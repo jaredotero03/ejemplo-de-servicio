44e17fd60b284854f0990637e8b4836fdf3daa6e
Johan Jared Otero Guzmán <jaredotero03@gmail.com>
2026-09-21 03:41:05 -0600

    servicio que devuelve Json v1.0

---
+++ servicio que devuelve Json/api/devuelve.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . "/../libservidorphp/devuelveJson.php";
+
+devuelveJson([
+ "nombre" => "pp",
+ "mensaje" => "\nAngel Gallagert Trejo Garrido\nCarvente Miguel Isai\nAvila Peralta Diego Rafael\nTabarez López Héctor Jesús\nGonzalez Rivera Axel\nRamírez López Marilú\nPérez Arellano Ariana\nContreras Gama Tadeo Habith\nOrtega Maldonado Rodrigo\nJohan Jared Otero Guzmán\nGuzman Sanchez Luis Enrique",
+]);
