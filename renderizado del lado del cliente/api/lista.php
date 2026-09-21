667c04187aa7613966352d54c064f87895022624
Johan Jared Otero Guzmán <jaredotero03@gmail.com>
2026-09-21 03:49:10 -0600

    renderizado del lado del cliente v1.0

---
+++ renderizado del lado del cliente/api/lista.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . "/../libservidorphp/manejaErrores.php";
+require_once __DIR__ . "/../libservidorphp/devuelveJson.php";
+
+devuelveJson([
+ [
+  "nombre" => "pp",
+  "color" => "azul"
+ ],
+ [
+  "nombre" => "kq",
+  "color" => "rojo"
+ ],
+ [
+  "nombre" => "tt",
+  "color" => "rosa"
+ ],
+ [
+  "nombre" => "bb",
+  "color" => "azul"
+ ]
+]);
